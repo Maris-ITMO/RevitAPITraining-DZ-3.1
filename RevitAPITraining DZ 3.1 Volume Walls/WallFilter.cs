8447f8d28033a870d7d5793c90b2800d9b2a80d5
Марис <122129172+Maris-ITMO@users.noreply.github.com>
2023-04-15 08:48:43 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining DZ 3.1 Volume Walls/WallFilter.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_DZ_3._1_Volume_Walls
+{
+    internal class WallFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return elem is Wall;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
